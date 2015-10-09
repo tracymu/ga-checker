f8975b8c607d3df6e215cf8436c1890197a25fc7
tracymu <tracy.musung@moomumedia.com>
2015-10-10 10:34:50 +1100

    Refactor to take more URLs to check

--- scrape.rb
+++ scrape.rb
@@ -7,9 +7,23 @@ def scrape_page(url)
   begin
     doc = Nokogiri.HTML(open(url))
   rescue Exception => e
-    send_page_down_mail
-    puts "The page is down"
+    send_page_down_mail(url)
+    puts "#{url} is down"
     exit
+  end
+  # doc = scrape_page(cf_url)
+  body = doc.xpath("//body")
+  body = body.to_s
+  send_mail(body, url)
+end
+
+def send_mail(body, url)
+  if body.include? "tagmanager"
+     puts "#{url} includes the tag"
+     send_includes_mail(url)
+  else
+    puts "#{url} doesn't include the tag"
+    send_excludes_mail(url)
   end
 end
 
@@ -25,46 +39,43 @@ Mail.defaults do
   }
 end
 
-def send_includes_mail
+def send_includes_mail(url)
   Mail.deliver do
     to 'aidan.moore@moomumedia.com'
     from 'tracy.musung@moomumedia.com'
-    subject 'Capital Finance Tag Manager found'
+    subject "#{url} Tag Manager found"
     body 'Seems to still be on there'
   end
 end
 
-def send_excludes_mail
+def send_excludes_mail(url)
   Mail.deliver do
     to 'aidan.moore@moomumedia.com'
     from 'tracy.musung@moomumedia.com'
-    subject 'Capital Finance Tag Manager not found'
-    body 'Go check www.capitalfinance.com.au for Tag Manager'
+    subject "#{url} Tag Manager not found"
+    body "Go check #{url} for Tag Manager"
   end
 end
 
-def send_page_down_mail
+def send_page_down_mail(url)
   Mail.deliver do
     to 'aidan.moore@moomumedia.com'
     from 'tracy.musung@moomumedia.com'
-    subject 'Is Capital Finance Site Down?'
+    subject 'Is #{url} Down?'
     body 'Daily checker could not reach page'
   end
 end
 
-cf_url = "http://www.capitalfinance.com.au"
-doc = scrape_page(cf_url)
-body = doc.xpath("//body")
-body = body.to_s
-
-
-if body.include? "tagmanager"
-   puts "includes"
-   send_includes_mail
-else
-  # puts "doesn't include"
-  send_excludes_mail
+def urls_to_check
+  %w(http://www.eccopoolfencing.com.au http://www.capitalfinance.com.au)
 end
 
 
+def check_all
+  urls_to_check.each do |url|
+    scrape_page(url)
+  end
+end
+
+check_all
 
